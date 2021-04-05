026b67dd2d79f275991607841b2736aa8684aae7
Bill Flynn <bill.flynn@jax.org>
2021-04-05 18:01:25 -0400

    Infercnv debugging 3

--- scripts/_infercnv.R
+++ scripts/_infercnv.R
@@ -4,8 +4,6 @@ library(infercnv)
 
 args = commandArgs(trailingOnly=TRUE)
 data_dir <- args[1]
-print(args)
-print(data_dir)
 
 setwd(data_dir)
 
@@ -14,12 +12,16 @@ gene_file <- paste(data_dir, "gene_pos2.txt", sep="/")
 anno_file <- paste(data_dir, "anno.txt", sep="/")
 out_dir <- paste(data_dir, "outputs-no-hmm", sep="/")
 
+annos <- read.table(anno_file)
+anno_types <- unique(annos[,2])
+normals <- as.character(anno_types[grep("^normal_", anno_types)])
+
 infercnv_obj <- CreateInfercnvObject(
     raw_counts_matrix=counts_file,
     annotations_file=anno_file,
     delim="\t",
     gene_order_file=gene_file,
-    ref_group_names=c("normal")
+    ref_group_names=normals
 )
 
 infercnv_obj <- infercnv::run(
